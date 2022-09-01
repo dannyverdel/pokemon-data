c0821031ea71c510cc57fe482619b829e3ea1140
Danny Verdel <dannyverdel@Dannys-MacBook-Pro.local>
2022-09-01 13:13:58 +0200

    Update action

--- update.js
+++ update.js
@@ -2,6 +2,7 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const pokemon = require('pokemontcgsdk')
 const fs = require('fs')
+const { exec } = require("child_process");
 
 const GetSets = async () => {
     const sets = await pokemon.set.all()
@@ -11,7 +12,7 @@ const GetSets = async () => {
 const AssignCards = async (sets) => {
     try {
         const result = []
-        for (let i = 0; i < 2; i++) {
+        for (let i = 0; i < sets.length; i++) {
             console.log(sets[i].id)
             const cards = await pokemon.card.where({ q: `set.id:${sets[i].id}` })
             sets[i].cards = cards.data
@@ -38,23 +39,63 @@ const AssignCards = async (sets) => {
 
 function delay(time) {
     return new Promise(resolve => setTimeout(resolve, time));
-  }
+}
 
 const main = async () => {
     const sets = await GetSets()
     const result = await AssignCards(sets)
 
-    if(!result) {
+    if (!result) {
         return
     }
 
-    const json = JSON.stringify(result)
+    const json = JSON.stringify(result, null, 2)
 
     return json
 }
 
 try {
-    main().then(data => core.setOutput("sets", data))
+    main().then(data => {
+        fs.writeFile('./data/data.json', data, (err, data) => {
+            if(err) {
+                core.setFailed(err.message)
+                return
+            }
+        })
+        core.setOutput("sets", data)
+    })
+
+    exec("git add .", (error, stdout, stderr) => {
+        if (error) {
+            core.setOutput(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            core.setOutput(`stderr: ${stderr}`);
+            return;
+        }
+    });
+    exec('git commit -m "Update data"', (error, stdout, stderr) => {
+        if (error) {
+            core.setOutput(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            core.setOutput(`stderr: ${stderr}`);
+            return;
+        }
+    });
+    exec("git push", (error, stdout, stderr) => {
+        if (error) {
+            core.setOutput(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            core.setOutput(`stderr: ${stderr}`);
+            return;
+        }
+    });
+    
     return
 } catch (error) {
     core.setFailed(error.message);
